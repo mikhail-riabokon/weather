860ded2e123aee11fe7afcfaf2bc704ffb777e2f
mikhail-riabokon <mikhail.riabokon@gmail.com>
2017-01-23 16:01:18 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>App 1</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
