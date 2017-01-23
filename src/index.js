0cd5abdfa4384c9f1c291b1fdbfd3408f2bf10f4
mikhail-riabokon <mikhail.riabokon@gmail.com>
2017-01-23 16:53:12 +0200

    init redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import { browserHistory } from 'react-router'
+import { browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import routes from './routes';
+import store from './store';
 
 ReactDOM.render(
-  <Router routes={routes} history={browserHistory} />,
+  <Provider store={store}>
+    <Router routes={routes} history={browserHistory} />
+  </Provider>,
   document.getElementById('root')
 );
