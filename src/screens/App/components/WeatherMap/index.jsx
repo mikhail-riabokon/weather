42cc3395f6dfa8667b232661b213f8a49180cc6e
mikhail-riabokon <mikhail.riabokon@gmail.com>
2017-02-20 22:01:48 +0200

    map usage

--- src/screens/App/components/WeatherMap/index.jsx
+++ src/screens/App/components/WeatherMap/index.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import GoogleMap from 'google-map-react';
 import { filteredResultsSelector } from '../../../../selectors';
+
+const NETHERLAND_CORDS = {
+  lat: 51.8937201,
+  lng: 4.3921683,
+};
+
+function ListItem(props) {
+  const styles = {
+    height: 10,
+    width: 10,
+    backgroundColor: '#000',
+    borderRadius: '50%',
+  };
+
+  return (
+    <div style={ styles }></div>
+  );
+}
 
 class WeatherMap extends React.Component {
   render() {
+    const bootstrapURLKeys = {
+      key: 'AIzaSyCEg2rGFn2wDEv7n8FIrHeqADtg920ZzZc',
+      language: 'en',
+    };
+
     return (
-      <div>
-        <h1>WeatherMap</h1>
-        <pre>
-          { JSON.stringify(this.props.filteredPlaces, null, 4) }
-        </pre>
+      <div style={{ height: 800 }}>
+        <GoogleMap
+          bootstrapURLKeys={bootstrapURLKeys}
+          defaultCenter={NETHERLAND_CORDS}
+          defaultZoom={7}
+        >
+          { this.props.filteredPlaces.map((place, index) => {
+            return <ListItem key={index} lat={place.latitude} lng={place.longitude} {...place} />
+          }) }
+        </GoogleMap>
       </div>
     );
   }
